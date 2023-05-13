9bfb32a2ba7a5508c766a66500667f5c5181dff7
Arjunkvl <arjunaskvl@gmail.com>
2023-05-13 08:21:27 +0530

    added splash screen transport to home

--- lib/Screen/screen_splash.dart
+++ lib/Screen/screen_splash.dart
@@ -1,10 +1,41 @@
 import 'package:flutter/material.dart';
 
-class Splash extends StatelessWidget {
+import 'Screen_home.dart';
+
+class Splash extends StatefulWidget {
   const Splash({super.key});
 
   @override
+  State<Splash> createState() => _SplashState();
+}
+
+class _SplashState extends State<Splash> {
+  @override
+  void initState() {
+    gotoHome(context);
+    super.initState();
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return const Center(child: Text('S O U N D'));
+    return const Scaffold(
+      backgroundColor: Color(0xffEFEFEF),
+      body: Center(
+          child: Text(
+        'S O U N D',
+        style: TextStyle(
+          fontFamily: 'font',
+          fontWeight: FontWeight.bold,
+          fontSize: 30,
+          color: Colors.black,
+        ),
+      )),
+    );
+  }
+
+  Future gotoHome(context) async {
+    await Future.delayed(const Duration(seconds: 1));
+    Navigator.of(context).pushReplacement(
+        MaterialPageRoute(builder: (BuildContext context) => const HomePage()));
   }
 }
